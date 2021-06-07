e050089476a0aedcf9ee28713d24555046ae82e6
Rahman Mammadov <rehman_mv@yahoo.com>
2021-06-07 16:19:43 +0400

    All files

---
+++ app/src/main/java/com/example/test_gltf_encryption/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.test_gltf_encryption
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
